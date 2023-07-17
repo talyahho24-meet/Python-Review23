ce6d53f3d27a55ec13a2abb9da79899296ab9de4
talyahho24-meet <139688490+talyahho24-meet@users.noreply.github.com>
2023-07-17 12:09:51 +0300

    i finished this on the 17.07 however  i forgot to commit

---
+++ pythonreview.py
@@ -0,0 +1,28 @@
+def create_youtube_video(cl):
+	cl = {"title":"how to speak to cats", "description": "If you think your cat hates you and you want it to like you, watch this video!", "likes":50000000000000000000, "dislikes": "nonexistance everyone loves cats",  "comments":"Miranda:finally we can worship cats properly as they truly deserve!"}
+	return cl 
+
+def likes (cl):
+	if "likes" in cl:
+		cl["likes"] =+1 
+	return cl 
+
+def dislikes (cl):
+	if "dislikes" in cl:
+		cl["dislikes"] =+1 
+	return cl
+
+def add_comment(youtubevideo):
+	youtubevideo = {"username":"shmeep", "comment_text":"something"}
+	if comments in cl:
+		cl['comments'][username] = comment_text
+    return video
+
+NEWVIDEO = {"title":"pop culture moments that eep me up at night", "likes":0, "dislikes":0, "comments":"kjhjhg"}
+NEWVIDEO = dislike(NEWVIDEO)
+NEWVIDEO = like(NEWVIDEO)
+NEWVIDEO = comment_text(NEWVIDEO)
+
+for i in range (495):
+		my_vieo = like(MEWVIDEO)
+print(NEWVIDEO)
