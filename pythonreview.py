d457ee0a5d6313878061c481be895997315802be
talyahho24-meet <139688490+talyahho24-meet@users.noreply.github.com>
2023-07-17 18:03:47 +0300

    nhgfvjfj

--- pythonreview.py
+++ pythonreview.py
@@ -1,19 +1,18 @@
-def create_youtube_video(cl):
-	cl = {"title":"how to speak to cats", "description": "If you think your cat hates you and you want it to like you, watch this video!", "likes":50000000000000000000, "dislikes": "nonexistance everyone loves cats",  "comments":"Miranda:finally we can worship cats properly as they truly deserve!"}
+def create_youtube_video(title, description):
+	cl = {"title": title , "description": description, "likes": 0, "dislikes": 0,  "comments":{}}
 	return cl 
 
 def likes (cl):
 	if "likes" in cl:
-		cl["likes"] =+1 
+		cl["likes"] +=1 
 	return cl 
 
 def dislikes (cl):
 	if "dislikes" in cl:
-		cl["dislikes"] =+1 
+		cl["dislikes"] +=1 
 	return cl
 
-def add_comment(youtubevideo):
-	youtubevideo = {"username":"shmeep", "comment_text":"something"}
+def add_comment(cl, username, comment_text):
 	if comments in cl:
 		cl['comments'][username] = comment_text
     return video
@@ -24,5 +23,6 @@ NEWVIDEO = like(NEWVIDEO)
 NEWVIDEO = comment_text(NEWVIDEO)
 
 for i in range (495):
-		my_vieo = like(MEWVIDEO)
+		my_video = like(MEWVIDEO)
 print(NEWVIDEO)
+
